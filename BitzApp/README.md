954bb4edf83ead5bdb78cd0eca85bf396bc5c9f2
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2020-02-17 15:46:11 +0000

    Working with APIs in NextJS.

---
+++ BitzApp/README.md
@@ -0,0 +1,27 @@
+# BitzApp
+
+> Sample Next.js app that returns the current Bitcoin value in USD, GBP and EUR from a designated API
+
+## Quick Start
+
+``` bash
+# Install dependencies
+npm install
+
+# Serve on localhost:3000
+npm run dev
+
+# Build for production
+npm run build
+```
+
+## App Info
+
+
+### Version
+
+1.0.0
+
+### License
+
+This project is licensed under the MIT License
---
+++ BitzApp/pages/index.js
@@ -0,0 +1,25 @@
+import Fetch from 'isomorphic-unfetch';
+import Layout from '../components/Layout';
+
+import Prices from '../components/Prices';
+
+const Index = (props) => (
+  <Layout>
+    <div>
+      <h1>Welcome to BlitzPrice</h1>
+      <p>Check current Bitcoin rate</p>
+      <Prices bpx={props.bpi}/>
+    </div>
+  </Layout>
+);
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+  const data = await res.json();
+
+  return {
+    bpx: data.bpi
+  };
+}
+
+export default Index;
